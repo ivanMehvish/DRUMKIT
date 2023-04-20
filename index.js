54d72ed571b8b94df69fb0f318a59add0f33236e
Mehvish Fatima <87938552+ivanMehvish@users.noreply.github.com>
2023-04-20 17:33:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
- for(var i =0 ; i<numberOfDrumButtons ; i++) {
-
-    document.querySelectorAll(".drum")[i].addEventListener("click" , function(){
-
-    var buttonInnerHTML =this.innerHTML;
-
-    makeSound(buttonInnerHTML);
-    buttonAnimation (buttonInnerHTML);
-    
-     });
-      }
      //detecting key press
      document.addEventListener("keypress" , function(event){
 
